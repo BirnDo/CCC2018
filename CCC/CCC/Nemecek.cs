6f167ea75eb2d0114bfc630f250e8f49ce0919c1
NemeDa <40146720160278@litec.ac.at>
2018-11-16 11:06:26 +0100

    sdf

--- CCC/CCC/Nemecek.cs
+++ CCC/CCC/Nemecek.cs
@@ -6,23 +6,25 @@ using System.Threading.Tasks;
 
 namespace CCC
 {
+    public class Buildings
+    {
+        public int height;
+        public int size;
+    }
+
+
     static class Nemecek
     {
-        class Buildings
-        {
-            public int height;
-            public int size;
-        }
-
+        
         public static bool ifBuildings()
         {
             
         }
 
-        public static List<Buildings> SizeBuilding(int[,])
+        public static List<Buildings> SizeBuilding(int[,] )
         {
             List<Buildings> buildings = new List<Buildings>();
-
+            
 
 
             return buildings;
